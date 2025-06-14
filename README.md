33ee212854f5d29b1162fb23b20d9a00cf2fcfea
unDEFER <undefer@gmail.com>
2025-06-14 13:39:33 +0300

    Wipe out old version. Now Neparsy gets the new conception.

--- README.md
+++ README.md
@@ -1,39 +1,12 @@
-Neparsy is language for representation result of parsing of any language.
-Also it has original graphical representation and GUI for editing .np-files.
+Neparsy is generalized Abstract Syntax Tree.
+
+The goal of parsing is not find all syntax-errors, but
+try to get correct AST any way.
+
+The goal of converting to code is not get correct code, but
+show AST-tree in understandable format for human.
 
 # Compiling
 To compile from D-sources you can use `dub`:
 
     $ dub build
-
-# Neparsy branch
-Use `git checkout neparsy` to switch to neparsy version of repository
-
-# Control
-Arrows -- navigation
-
-Comma -- add child
-
-Space -- add sibling
-
-Dot -- move down current node to child
-
-Ctrl+Comma -- add postfix child/extend influence to the left from parent
-
-Ctrl+Dot -- add postfix child/decrease influence to the right from parent
-
-Shift+Left, Shift+Right -- move current node to the left/right
-
-Ctrl + Arrows -- navigate by fields (when diagram visible on bottom of the left panel)
-
-Del -- remove node and all descendants
-
-Ctrl+Backspace -- remove descendants
-
-Ctrl+S -- save to neparsy-format
-
-Ctrl+D -- save to .d-file (@D modules)
-
-Ctrl+L -- make lexer module from syntax (@Lexer modules)
-
-Enter/Escape -- exit from edit mode
--- src/main.d
+++ src/main.d
@@ -4,15 +4,6 @@
 
 module main;
 
-import iface;
-
-import gio.Application : GioApplication = Application;
-import gio.FileT;
-import gtk.Application;
-import gtk.ApplicationWindow;
-import gtk.Widget;
-import gdk.Event;
-import gdk.Keymap;
 import std.stdio;
 import std.array;
 import std.string;
@@ -24,357 +15,79 @@ import std.math;
 import std.algorithm;
 import std.uni;
 import std.file;
-import expression;
-import node;
-import lexer;
-import parser;
 
-class MyWindow : ApplicationWindow
+enum Style
 {
-    public this (GtkApplicationWindow* gtkApplicationWindow, bool ownedRef = false)
-    {
-        super(gtkApplicationWindow, ownedRef);
-    }
-
-    public this (Application application)
-    {
-        super(application);
-        addEvents(EventMask.KEY_PRESS_MASK | EventMask.BUTTON_PRESS_MASK);
-		addOnKeyPress(&keyPressCallback);
-		addOnButtonPress(&buttonPressCallback);
-		addOnScroll(&scrollCallback);
-    }
-
-    protected:
-    bool keyPressCallback(Event event, Widget widget)
-    {
-        uint val;
-        event.getKeyval(val);
-        dchar unicode = cast(dchar) Keymap.keyvalToUnicode(val);
-
-        GdkModifierType state;
-        event.getState(state);
-
-        if (state & GdkModifierType.SHIFT_MASK && val == 65361) /*LEFT*/
-        {
-            //write("S<");
-            IFACE.moveleft();
-        }
-        else if (state & GdkModifierType.SHIFT_MASK && val == 65363) /*RIGHT*/
-        {
-            //write("S>");
-            IFACE.moveright();
-        }
-        else if (val == 65535)
-        {
-            //write("Del");
-            IFACE.del();
-        }
-        else if (val == 65307 /*Escape*/ || val == 65293 /*Enter*/)
-        {
-            //write('\n');
-            IFACE.escape();
-        }
-        else if (unicode == ' ')
-        {
-            //write(unicode);
-            IFACE.space();
-        }
-        else if (state & GdkModifierType.CONTROL_MASK)
-        {
-            if (unicode == ',')
-            {
-                //write("^<");
-                IFACE.less();
-            }
-            else if (unicode == '.')
-            {
-                //write("^>");
-                IFACE.greater();
-            }
-            else if (unicode == 's')
-            {
-                IFACE.save();
-            }
-            else if (unicode == 'd')
-            {
-                IFACE.saveD();
-            }
-            else if (unicode == 'f')
-            {
-                IFACE.fixIndent();
-            }
-            else if (unicode == 'c')
-            {
-                //write('^c', unicode);
-                IFACE.copy();
-            }
-            else if (unicode == 'x')
-            {
-                //write('^x', unicode);
-                IFACE.copy();
-                IFACE.del();
-            }
-            else if (unicode == 'v')
-            {
-                //write('^v', unicode);
-                IFACE.insert();
-            }
-            else if (unicode == 'l')
-            {
-                //write('^l', unicode);
-                IFACE.toLexer();
-            }
-            else if (unicode == 'w')
-            {
-                //write('^w', unicode);
-                IFACE.changeView();
-            }
-            else if (unicode == '=')
-            {
-                IFACE.sx2 /= 1.5;
-                IFACE.sy2 /= 1.5;
-                IFACE.redraw();
-            }
-            else if (unicode == '-')
-            {
-                IFACE.sx2 *= 1.5;
-                IFACE.sy2 *= 1.5;
-                IFACE.redraw();
-            }
-            else if (val == 65288)
-            {
-                //write("C<-");
-                IFACE.remove();
-            }
-            else if (val == 65361) /*LEFT*/
-            {
-                //write("C<");
-                IFACE.field_left();
-            }
-            else if (val == 65363) /*RIGHT*/
-            {
-                //write("C>");
-                IFACE.field_right();
-            }
-            else if (val == 65364) /*DOWN*/
-            {
-                //write("Cv");
-                IFACE.field_down();
-            }
-            else if (val == 65362) /*UP*/
-            {
-                //write("C^");
-                IFACE.field_up();
-            }
-        }
-        else if (val == 65288)
-        {
-            //write("<-");
-            IFACE.backspace();
-        }
-        else if (val == 65361) /*LEFT*/
-        {
-            //write("<");
-            IFACE.left();
-        }
-        else if (val == 65363) /*RIGHT*/
-        {
-            //write(">");
-            IFACE.right();
-        }
-        else if (val == 65364) /*DOWN*/
-        {
-            //write("v");
-            IFACE.down();
-        }
-        else if (val == 65362) /*UP*/
-        {
-            //write("^");
-            IFACE.up();
-        }
-        else if (unicode == '.')
-        {
-            //write(unicode);
-            IFACE.dot();
-        }
-        else if (unicode == ',')
-        {
-            //write(unicode);
-            IFACE.comma();
-        }
-        else if ( isAlphaNum(unicode) || isPunctuation(unicode) )
-        {
-            //write(unicode);
-            IFACE.print(unicode);
-        }
-
-        IFACE.redraw();
-
-        return false;
-    }
-
-    bool buttonPressCallback(Event event, Widget widget)
-    {
-        uint button;
-        event.getButton(button);
-        double x, y;
-        event.getCoords(x, y);
-
-        //writefln("%s. %sx%s", button, x, y);
-        return IFACE.click(button, x, y);
-    }
-
-    bool scrollCallback(Event event, Widget widget)
-    {
-        double deltaX, deltaY;
-        event.getScrollDeltas(deltaX, deltaY);
-        return IFACE.scroll(deltaX, deltaY);
-    }
+    Unknown,
+    C,
+    CPP,
+    D,
+    Java,
+    Rust,
+    Python
 }
 
-Iface IFACE;
+Style[string] name2style = ["C": Style.C, "C++": Style.CPP, "D": Style.D, "Java": Style.Java, "Rust": Style.Rust, "Python": Style.Python];
 
-struct BranchInfo
+void usage(string execname)
 {
-    int maxdeep;
-    real maxx;
-    real maxy;
-    Expression fig;
+    stderr.writefln("usage: %s [--style <Language Name>] <source> <destination> -- for converting to/from neparsy format\n"
+            ~"where neparsy-files must have '.np' extension\n"
+            ~"Supported styles: C/C++/D/Java/Rust/Python", execname);
 }
 
 int main(string[] args)
 {
-    if (args.length > 1)
+    string execname = args[0];
+
+    if (args.length < 3 || args[1] == "-h" || args[1] == "--help")
     {
-        if (args[1] == "-h" || args[1] == "--help")
-        {
-            writefln("usage: %s [file1] [file2]... -- for editing files in GUI\n"
-                    ~"   or: %s -c <source> <destination> -- for converting between neparsy/D formats\n"
-                    ~"where files must have '.np' or '.d' extension", args[0], args[0]);
-            return 0;
-        }
-        else if (args[1] == "-c" && args.length == 4)
-        {
-            Expression expr;
-            Node node;
-            if (args[2].endsWith(".d"))
-            {
-                Lexer lex;
-                lex.file = readText(args[2]);
-                Parser pars = new Parser;
-                pars.lexer = lex;
-                expr = pars.parse();
-                expr.fixParents();
-                expr.calcIndentRecursive(1);
-                expr.cutIndent(1);
-            }
-            else if (args[2].endsWith(".np"))
-            {
-                string mod = readText(args[2]);
-                //expr = new Expression(mod.idup);
-                node = new Node(mod.idup, args[2]);
-            }
-            else
-            {
-                writefln("Can't convert file with unknown extension: `%s`", args[2]);
-                return 1;
-            }
-
-            string savestr;
-
-            if (args[3].endsWith(".d"))
-            {
-                //if (args[2].endsWith(".np"))
-                node.Debug();
-                node.genDText();
-                node.Debug(1);
-                savestr = node.toText(1);
-            }
-            else if (args[3].endsWith(".np"))
-            {
-                //expr.save();
-                //savestr = expr.saveText(0, true);
-                node.Debug();
-                node.genText();
-                savestr = node.toText(0);
-            }
-            else
-            {
-                writefln("Can't convert to file with unknown extension: `%s`", args[3]);
-                return 1;
-            }
-
-            auto file = File(args[3], "w");
-            file.write(savestr);
-            return 0;
-        }
+        usage(execname);
+        return 1;
     }
 
-	Application application;
+    Style style;
 
-	void activateClock(GioApplication app)
-	{
-		MyWindow win = new MyWindow(application);
-
-		win.setTitle("Neparsy");
-		win.setDefaultSize( 640, 480 );
-
-        real x = 0.0;
-        real y = 0.0;
-
-        int state = 0;
-        BranchInfo[] branches;
-
-        Expression root = new Expression();
-        root.type = "root";
-
-        foreach (i, arg; args[1..$])
+    if (args[1] == "-s" || args[1] == "--style")
+    {
+        if (args.length < 5)
         {
-            if (arg.endsWith(".d"))
-            {
-                Lexer lex;
-                lex.file = readText(arg);
-                Parser pars = new Parser;
-                pars.lexer = lex;
-                Expression expr = pars.parse();
-                expr.fixParents();
-
-                expr.parent = root;
-                expr.operator = arg[0..$-2]~".np";
-                expr.type = "*";
-                expr.index = root.arguments.length;
-                root.arguments ~= expr;
-            }
-            else if (arg.endsWith(".np"))
-            {
-                string mod = readText(arg);
-                Expression expr = new Expression(mod);
-                expr.parent = root;
-                expr.operator = arg;
-                expr.index = i;
-                root.arguments ~= expr;
-            }
-            else
-            {
-                writefln("Skip file with unknown extension: `%s`", arg);
-            }
+            stderr.writefln("Not enough arguments");
+            usage(execname);
+            return 1;
+        }
+        
+        if (args[2] !in name2style)
+        {
+            stderr.writefln("Unknown style %s", args[2]);
+            usage(execname);
+            return 1;
         }
 
-		IFACE = new Iface(root);
-		win.add(IFACE);
-		IFACE.show();
-		win.showAll();
-	}
+        style = name2style[args[2]];
 
-	void handleOpen(FileIF[] files, string name, GioApplication app)
-	{
-	    activateClock(app);
+        args = args[2..$];
     }
 
-	application = new Application("org.gtkd.demo.cairo.iface", GApplicationFlags.HANDLES_OPEN);
-	application.addOnOpen(&handleOpen);
-	application.addOnActivate(&activateClock);
-	return application.run(args);
+    string input = args[1];
+    string output = args[2];
+
+    if ((input.endsWith(".np")?1:0) + (output.endsWith(".np")?1:0) != 1)
+    {
+        stderr.writefln("Exactly one of files must has .np-suffix");
+        usage(execname);
+        return 1;
+    }
+
+    if (input.endsWith(".np"))
+    {
+        writefln("OK. Here we will convert neparsy-file %s to %s with %s style", input, output, style);
+    }
+    else //if (output.endsWith(".np"))
+    {
+        writefln("OK. Here we will convert %s with %s style to %s neparsy-file", input, style, output);
+    }
+
+    return 0;
 }
 
